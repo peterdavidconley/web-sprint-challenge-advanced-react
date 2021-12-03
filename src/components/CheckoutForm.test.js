447a66b22d9be0aa002141d9ad486b21d8e2b1eb
Peter Conley <peterdavidconley@gmail.com>
2021-12-03 08:08:14 -0500

    added getters for field input values by text

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -33,7 +33,7 @@ test("shows success message on submit with form details", () => {
     userEvent.type(fNameInput, "Peter")
     userEvent.type(lNameInput, "Conley")
     userEvent.type(addressInput, "315 S Cayuga Rd")
-    userEvent.type(cityInput, "Williamsville")
+    userEvent.type(cityInput, "Buffalo")
     userEvent.type(stateInput, "NY")
     userEvent.type(zipInput, "14221")
 
@@ -42,12 +42,26 @@ test("shows success message on submit with form details", () => {
     const button = screen.getByRole("button");
     userEvent.click(button);
 
-    // 4. Getter for success message
+    // 4. Getter for success message & input values
 
     const successMessage = screen.queryByText('You have ordered some plants!')
+    const firstName = screen.getByText('Peter')
+    const lastName = screen.getByText('Conley')
+    const address = screen.getByText('315 S Cayuga Rd')
+    const city = screen.getByText('Buffalo')
+    const state = screen.getByText('NY')
+    const zip = screen.getByText('14221')
+
 
     // Assert
 
     expect(successMessage).toBeInTheDocument();
+    expect(firstName).toBeInTheDocument();
+    expect(lastName).toBeInTheDocument();
+    expect(address).toBeInTheDocument();
+    expect(city).toBeInTheDocument();
+    expect(state).toBeInTheDocument();
+    expect(zip).toBeInTheDocument();
+
 
 });
