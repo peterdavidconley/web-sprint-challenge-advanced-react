d73293a41872a08ad660a255ee0235b09f9a21af
Peter Conley <peterdavidconley@gmail.com>
2021-12-03 06:32:25 -0500

    copied and pasted all stateful logic into Hook function

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react'
+
+export const useForm = initialValue => {
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+    return [ handleSubmit, values, handleChanges, showSuccessMessage ]
+}
